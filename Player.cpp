413879b4c5e3360850dc341ec4b1b84f2ea4f0c7
slier81 <slier81@gmail.com>
2020-07-21 11:00:50 +0800

    Add player class 21072020

---
+++ Player.cpp
@@ -0,0 +1,161 @@
+#include "include/Player.h"
+
+Player::Player() {
+    speed = START_SPEED;
+    health = START_HEALTH;
+    maxHealth = START_HEALTH;
+
+    /* Associate a texture with the sprite */
+    texture.loadFromFile("asset/graphic/player.png");
+    sprite.setTexture(texture);
+
+    sprite.setOrigin(25, 25);
+}
+
+void Player::spawn(sf::IntRect _arena, sf::Vector2f _resolution, int _tileSize) {
+    /* Place the player in the middle of the arena*/
+    position.x = arena.width / 2;
+
+    position.y = arena.height / 2;
+
+    /* Copy the details of the arena to the players arena*/
+    arena.left = _arena.left;
+    arena.width = _arena.width;
+    arena.top = _arena.top;
+    arena.height = _arena.height;
+
+    /* Remember how big the tiles are ins this arena */
+    tileSize = _tileSize;
+
+    /* Store the resolution for future use */
+    resolution.x = _resolution.x;
+    resolution.y = _resolution.y;
+}
+
+sf::Time Player::getLastHitTime() {
+    return lastHit;
+}
+
+bool Player::hit(sf::Time timeHit) {
+    /* 2 tenths of second */
+    if (timeHit.asMilliseconds() - lastHit.asMilliseconds() > 200) {
+        lastHit = timeHit;
+        health -= 10;
+        return true;
+    }
+
+    return false;
+}
+
+sf::FloatRect Player::getPosition() {
+    return sprite.getGlobalBounds();
+}
+
+sf::Vector2f Player::getCenter() {
+    return position;
+}
+
+float Player::getRotation() {
+    return sprite.getRotation();
+}
+
+sf::Sprite Player::getSprite() {
+    return sprite;
+}
+
+int Player::getHealth() {
+    return health;
+}
+
+void Player::moveLeft() {
+    leftPressed = true;
+}
+
+void Player::moveRight() {
+    rightPressed = true;
+}
+
+void Player::moveUp() {
+    upPressed = true;
+}
+
+void Player::moveDown() {
+    downPressed = true;
+}
+
+void Player::stopLeft() {
+    leftPressed = false;
+}
+
+void Player::stopRight() {
+    rightPressed = false;
+}
+
+void Player::stopUp() {
+    upPressed = false;
+}
+
+void Player::stopDown() {
+    downPressed = false;
+}
+
+void Player::update(float elapsedTime, sf::Vector2i mousePosition) {
+    if (upPressed) {
+        position.y -= speed * elapsedTime;
+    }
+
+    if (downPressed) {
+        position.y += speed * elapsedTime;
+    }
+
+    if (rightPressed) {
+        position.x += speed * elapsedTime;
+    }
+
+    if (leftPressed) {
+        position.x -= speed * elapsedTime;
+    }
+
+    sprite.setPosition(position);
+
+    /* keep the player in the arena */
+    if (position.x > arena.width - tileSize) {
+        position.x = arena.width - tileSize;
+    }
+
+    if (position.x < arena.left + tileSize) {
+        position.x = arena.left + tileSize;
+    }
+
+    /* If player bottom position is way too bottom, put player at the most bottom of the screen*/
+    if (position.y > arena.height - tileSize) {
+        position.y = arena.height - tileSize;
+    }
+
+    /* If player top position is way too top, put player at the most top of the screen*/
+    if (position.y < arena.top + tileSize) {
+        position.y = arena.top + tileSize;
+    }
+
+    /* Calculate the angle the player is facing */
+    float angle = (atan2(mousePosition.y - resolution.y / 2,
+                         mousePosition.x - resolution.x / 2) *
+                   180) /
+                  3.141;
+
+    sprite.setRotation(angle);
+}
+
+void Player::upgradeHealth() {
+    /* 20% max health upgrade */
+    maxHealth += START_HEALTH * 0.2;
+}
+
+void Player::increaseHealthLevel(int amount) {
+    health += amount;
+
+    /*If health > maxHealth, cap it to maxHealth */
+    if (health > maxHealth) {
+        health = maxHealth;
+    }
+}
---
+++ include/Player.h
@@ -0,0 +1,99 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Player {
+    const float START_SPEED = 200;
+    const float START_HEALTH = 100;
+
+    /* Where is the player */
+    sf::Vector2f position;
+
+    /* Ofcourse we will need a sprite */
+    sf::Sprite sprite;
+
+    /* Add a texture */
+    sf::Texture texture;
+
+    /* What is the screen resolution */
+    sf::Vector2f resolution;
+
+    /* What size is the current arena */
+    sf::IntRect arena;
+
+    /* How big is each tile of the arena */
+    int tileSize;
+
+    /* Which directions is the player currently moving in */
+    bool upPressed;
+    bool downPressed;
+    bool leftPressed;
+    bool rightPressed;
+
+    /* How much health has the player got */
+    int health;
+
+    /* What is the maximum health the player can have */
+    int maxHealth;
+
+    /* When was the player last hit*/
+    sf::Time lastHit;
+
+    /* Speed in pixels per seconds */
+    float speed;
+
+   public:
+    Player();
+
+    void spawn(sf::IntRect arena, sf::Vector2f resolution, int tileSize);
+
+    /* Handle the player getting hit by a zombie */
+    bool hit(sf::Time timeHit;);
+
+    /* How long ago was the palyer last hit */
+    sf::Time getLastHitTime();
+
+    /* Where is the player */
+    sf::FloatRect getPosition();
+
+    /* Where is the center of the player */
+    sf::Vector2f getCenter();
+
+    /* Which angle is the player facing */
+    float getRotation();
+
+    /* Return a copy of the sprite to the caller */
+    sf::Sprite getSprite();
+
+    /* How much health has the player currently got */
+    int getHealth();
+
+    /* The next four functions move the player */
+    void moveLeft();
+
+    void moveRight();
+
+    void moveUp();
+
+    void moveDown();
+
+    /* Stop the player moving in a specific direction */
+    void stopLeft();
+
+    void stopRight();
+
+    void stopUp();
+
+    void stopDown();
+
+    /* We will call this function once every frame */
+    void update(float elapsedTime, sf::Vector2i mouseposition);
+
+    /* Give player a speed boost */
+    void upgradeSpeed();
+
+    /* Give the player some health */
+    void upgradeHealth();
+
+    /* Increase the maximum amount of health the player can have*/
+    void increaseHealthLevel(int amount);
+};
