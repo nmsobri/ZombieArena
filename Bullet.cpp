4d0af7e1f47d2cbf0b846a6c6c77f20944b69e66
slier81 <slier81@gmail.com>
2020-07-28 11:48:02 +0800

    Add bullet class 28072020

---
+++ Bullet.cpp
@@ -0,0 +1,83 @@
+#include "include/Bullet.h"
+
+Bullet::Bullet() {
+    bulletShape.setSize(sf::Vector2f(2, 2));
+}
+
+Bullet::~Bullet() {
+}
+
+void Bullet::stop() {
+    isInFlight = false;
+}
+
+bool Bullet::isBulletInFlight() {
+    return isInFlight;
+}
+
+/* Launch a new bullet */
+void Bullet::shoot(float startX, float startY, float targetX, float targetY) {
+    /* Keep track of the bullet*/
+    isInFlight = true;
+    position.x = startX;
+    position.y = startY;
+
+    /* Calculate the gradient of the flight path */
+    float gradient = (startX - targetX) / (startY - targetY);
+
+    /* Any gradient less than zero needs to be negative */
+    if (gradient < 0) {
+        gradient *= -1;
+    }
+
+    /* Calculate the ratio between x and t */
+    float ratioXY = bulletSpeed / (1 + gradient);
+
+    /* Set the "speed" horizontally and vertically */
+    bulletDistanceY = ratioXY;
+    bulletDistanceX = ratioXY * gradient;
+
+    /* Point the bullet in the right direction */
+    if (targetX < startX) {
+        bulletDistanceX *= -1;
+    }
+
+    if (targetY < startY) {
+        bulletDistanceY *= -1;
+    }
+
+    /* Set a max range of 1000 pixels */
+    float range = 1000;
+    minX = startX - range;
+    maxX = startX + range;
+    minY = startY - range;
+    maxY = startY + range;
+
+    /* Position the bullet ready to be drawn */
+    bulletShape.setPosition(position);
+}
+
+/* Tell the calling code where the bullet is in the world */
+sf::FloatRect Bullet::getPosition() {
+    return bulletShape.getGlobalBounds();
+}
+
+/* Return the actual shape (for drawing) */
+sf::RectangleShape Bullet::getShape() {
+    return bulletShape;
+}
+
+/* Update the bullet each frame */
+void Bullet::update(float deltaTime) {
+    /* Update the bullet position variables */
+    position.x += bulletDistanceX * deltaTime;
+    position.y += bulletDistanceY * deltaTime;
+
+    /* Move the bullet */
+    bulletShape.setPosition(position);
+
+    if (position.x < minX || position.x > maxX ||
+        position.y < minY || position.y > maxY) {
+        isInFlight = false;
+    }
+}
---
+++ include/Bullet.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+class Bullet {
+    sf::Vector2f position;
+    sf::RectangleShape bulletShape;
+    bool isInFlight = false;
+    float bulletSpeed = 1000.0f;
+
+    /* What fraction of 1 pixel does the bullet travel */
+    /* Horizontally and vertically each frame */
+    /* These values will be derived from m_BulletSpeed */
+    float bulletDistanceX;
+    float bulletDistanceY;
+
+    /* Where is this bullet headed to */
+    float targetX;
+    float targetY;
+
+    /* Some boundaries so the bullet doesn't fly forever */
+    float maxX;
+    float minX;
+    float maxY;
+    float minY;
+
+  public:
+    Bullet();
+
+    virtual ~Bullet();
+
+    void stop();
+
+    bool isBulletInFlight();
+
+    /* Launch a new bullet */
+    void shoot(float startX, float startY, float targetX, float targetY);
+
+    /* Tell the calling code where the bullet is in the world */
+    sf::FloatRect getPosition();
+
+    /* Return the actual shape (for drawing) */
+    sf::RectangleShape getShape();
+
+    /* Update the bullet each frame */
+    void update(float deltaTime);
+};
